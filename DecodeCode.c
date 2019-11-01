6bd72dee277649202927dbce6985bec0023d5b27
Erwan Fraisse <efraisse@cs.ucsb.edu>
2019-11-01 13:35:37 -0700

    only need to figure out signed now

--- DecodeCode.c
+++ DecodeCode.c
@@ -5,12 +5,12 @@ mipsinstruction decode(int value)
 	mipsinstruction instr;
 
 	// TODO: fill in the fields
-	instr.funct = (value & 0b111111);
-	instr.immediate = (value & 0b1111111111111111);
-	instr.rd = (value & 0b000000000000000001111100000000000);
-	instr.rt = (value & 0b00000000000111110000000000000000);
-	instr.rs = (value & 0b00000011111000000000000000000000);
-	instr.opcode = (value & 0b11111100000000000000000000000000);
+	instr.funct = (value & 0b111111) >> 0;
+	instr.immediate = (value & 0b1111111111111111) >> 0;
+	instr.rd = (value & 0b1111100000000000) >> 11;
+	instr.rt = (value & 0b111110000000000000000) >> 16;
+	instr.rs = (value & 0b11111000000000000000000000) >> 21;
+	instr.opcode = (value & 0b11111100000000000000000000000000) >> 26;
 
 	return instr;
 }
